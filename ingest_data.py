a2f4fab18b352711ec013200bbc7494e5cda732e
PhilippeDataScienc <philippe.acquier@gmail.com>
2023-10-02 13:45:13 +0200

    dockerisation of ingst_data.py script, v001

--- ingest_data.py
+++ ingest_data.py
@@ -16,7 +16,7 @@ def main(params):
 
     parquet_name = 'output.parquet'
 
-    os.system(f"wget {url} -O {parquet_name}")
+    os.system(f"curl {url} -o {parquet_name}")
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
@@ -33,13 +33,13 @@ if __name__=="__main__":
 
     parser = argparse.ArgumentParser(description='Ingest parquet data to Postgres.')
 
-    parser.add_argument('user', help='user name for postgres')
-    parser.add_argument('password', help='password for postgres')
-    parser.add_argument('host', help='host for postgres')
-    parser.add_argument('port', help='port for postgres')
-    parser.add_argument('db', help='database name for postgres')
-    parser.add_argument('table_name', help='name of the table where we will write the results to')
-    parser.add_argument('url', help='url of the parquet file')
+    parser.add_argument('--user', help='user name for postgres')
+    parser.add_argument('--password', help='password for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port for postgres')
+    parser.add_argument('--db', help='database name for postgres')
+    parser.add_argument('--table_name', help='name of the table where we will write the results to')
+    parser.add_argument('--url', help='url of the parquet file')
 
     args = parser.parse_args()
 
